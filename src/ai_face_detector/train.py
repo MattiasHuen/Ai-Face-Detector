00fd07b8396fa8a2c6a250f40f01686a1834198c
MattiasHuen <mattiashouen@hotmail.com>
2026-01-09 21:36:50 +0100

    Added folders and files

---
+++ src/ai_face_detector/train.py
@@ -0,0 +1,10 @@
+from ai_face_detector.model import Model
+from ai_face_detector.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
